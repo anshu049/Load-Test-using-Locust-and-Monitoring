51799e6bf47f95166e85fe251c69cacd4fdd2840
Anshu Kumar <95365748+anshu049@users.noreply.github.com>
2024-03-28 21:23:59 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,23 +1,10 @@
-# app.py
-
 from flask import Flask
-from prometheus_client import Counter, generate_latest, REGISTRY
-from flask import Response
 
 app = Flask(__name__)
 
-# Define a counter metric
-REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP Requests', ['method', 'endpoint', 'status'])
-
 @app.route('/')
-def hello_world():
-    REQUEST_COUNT.labels(method='GET', endpoint='/', status=200).inc()
-    return 'Hello, World!!!'
-
-@app.route('/metrics')
-def metrics():
-    return Response(generate_latest(REGISTRY), mimetype='text/plain')
+def index():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(host='0.0.0.0', port=5000)
